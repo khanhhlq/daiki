8bb9ca7149ac5bb8b09be2efbc4d0d9600751911
ooAkira <lee.quockhanh1107@gmail.com>
2022-03-27 14:25:26 +0700

    Delete Button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,45 +1,35 @@
-<script>
-      // 2. This code loads the IFrame Player API code asynchronously.
-    var tag = document.createElement('script');
 
-    tag.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
 
-      // 3. This function creates an <iframe> (and YouTube player)
-        //    after the API code downloads.
-        var player;
-        function onYouTubeIframeAPIReady() {
-            player = new YT.Player('player', {
-                height: '390',
-                width: '640',
-                videoId: 'M7lc1UVf-VE',
-                playerVars: {
-                    'playsinline': 1
-                },
-                events: {
-                    'onReady': onPlayerReady,
-                    'onStateChange': onPlayerStateChange
-                }
-            });
-      }
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-        // 4. The API will call this function when the video player is ready.
-        function onPlayerReady(event) {
-            event.target.playVideo();
-      }
-
-        // 5. The API calls this function when the player's state changes.
-        //    The function indicates that when playing a video (state=1),
-        //    the player should play for six seconds and then stop.
-        var done = false;
-        function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-            setTimeout(stopVideo, 6000);
-        done = true;
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: '5qap5aO4i9A',
+        playerVars: {
+            'autoplay': 0,
+            'controls': 1,
+            'playsinline': 1
+        },
+        events: {
+            'onReady': onPlayerReady,
         }
-      }
-        function stopVideo() {
-            player.stopVideo();
-      }
-</script>
+    });
+}
+
+function playVideo() {
+    player.playVideo();
+}
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+function stopVideo() {
+    player.stopVideo();
+}
