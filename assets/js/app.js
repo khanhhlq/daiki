01b87b0ce42c33a1503d8cdef039902079540356
ooAkira <lee.quockhanh1107@gmail.com>
2022-03-30 22:22:32 +0700

    Fix JS no music

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,42 +1,37 @@
 
 // 🌱 Code này tải IFrame Player API.
 let tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
 
 // 🌵 Khởi ạo biến để tương tác với video
 let player;
 
+tag.src = "https://www.youtube.com/iframe_api";
+
+// 💦 Xử lí API
 let firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // 💡 Hàm lấy dữ liệu video trên youtube về bằng id video
-function onYouTubeIframeAPIReady() {
+const onYouTubeIframeAPIReady = () => {
     player = new YT.Player('player', {
         height: '390',
         width: '640',
         videoId: '5qap5aO4i9A',
         playerVars: {
             'autoplay': 0,
-            'controls': 1,
-            'playsinline': 1
+            'controls': 1
         },
         events: {
-            'onReady': onPlayerReady,
+            'onReady': onPlayerReady
         }
     })
 }
 
 // 🔊 Bắt đầu phát
-const playVideo = () => {
-    player.playVideo()
-}
+const playVideo = () => player.playVideo()
 
 // 🔈 Tạm dừng 
-const pauseVideo = () => {
-    player.pauseVideo()
-}
+const pauseVideo = () => player.pauseVideo()
 
-
-const onPlayerReady = (event) => {
-    event.target.playVideo()
-}
+// 🔴 Phát ngay lần đầu tiên
+const onPlayerReady = (event) => event.target.playVideo()
