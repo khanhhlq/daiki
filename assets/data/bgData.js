4d9d071f8b2f10c626dcba2ae71899372584a49e
ooAkira <lee.quockhanh1107@gmail.com>
2022-04-13 22:05:22 +0700

    done code today

--- assets/data/bgData.js
+++ assets/data/bgData.js
@@ -1,4 +1,15 @@
-let bg = {
-    '1': "",
-    '2': ""
+let backGround = {
+    '01': "https://dc85enhu9zukf.cloudfront.net/gifs/lkceXNDw4Agryfrwz8.gif",
+    '02': "",
+    '03': "",
+    '04': "",
+    '05': "",
+    '06': "",
+    '07': "",
+    '08': "",
+    '09': "",
+    '10': "",
 }
+
+let image = document.getElementById("background")
+
