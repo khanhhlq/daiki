0b05f10412e4b2f208ada4bbfd6c015cddbc9c17
ooAkira <lee.quockhanh1107@gmail.com>
2022-04-14 20:26:07 +0700

    ADD data bg

--- assets/data/bgData.js
+++ assets/data/bgData.js
@@ -1,14 +1,14 @@
 let backGround = {
     '01': "https://dc85enhu9zukf.cloudfront.net/gifs/lkceXNDw4Agryfrwz8.gif",
-    '02': "",
-    '03': "",
-    '04': "",
-    '05': "",
-    '06': "",
-    '07': "",
-    '08': "",
-    '09': "",
-    '10': "",
+    '02': "https://dc85enhu9zukf.cloudfront.net/gifs/lkceXNDw4Agryfrwz8.gif",
+    '03': "https://dc85enhu9zukf.cloudfront.net/gifs/lkceXNDw4Agryfrwz8.gif",
+    '04': "https://dc85enhu9zukf.cloudfront.net/gifs/lkceXNDw4Agryfrwz8.gif",
+    '05': "https://dc85enhu9zukf.cloudfront.net/gifs/lkceXNDw4Agryfrwz8.gif",
+    '06': "https://dc85enhu9zukf.cloudfront.net/gifs/lkceXNDw4Agryfrwz8.gif",
+    '07': "https://dc85enhu9zukf.cloudfront.net/gifs/lkceXNDw4Agryfrwz8.gif",
+    '08': "https://dc85enhu9zukf.cloudfront.net/gifs/lkceXNDw4Agryfrwz8.gif",
+    '09': "https://dc85enhu9zukf.cloudfront.net/gifs/lkceXNDw4Agryfrwz8.gif",
+    '10': "https://dc85enhu9zukf.cloudfront.net/gifs/lkceXNDw4Agryfrwz8.gif",
 }
 
 let image = document.getElementById("background")
