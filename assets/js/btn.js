0fd66387c9292229b1a38b2eac34fc4c0121c5f1
ooAkira <lee.quockhanh1107@gmail.com>
2022-04-16 22:45:06 +0700

    fix bug play

--- assets/js/btn.js
+++ assets/js/btn.js
@@ -7,17 +7,25 @@ let nameSong = document.getElementById("name-song");
 
 btn1.addEventListener("click", () => {
     nameSong.innerHTML = "lofi hip hop radio - beats to relax/study to"
+    document.getElementById("pause").classList.add("displayNone")
+    document.getElementById("play").classList.remove("displayNone")
 })
 
 btn2.addEventListener("click", () => {
     nameSong.innerHTML = "coffee shop radio // 24/7 lofi hip-hop beats"
+    document.getElementById("pause").classList.add("displayNone")
+    document.getElementById("play").classList.remove("displayNone")
 })
 
 btn3.addEventListener("click", () => {
     nameSong.innerHTML = "Jazz Piano Music - Relaxing Slow Jazz 24/7 for Work, Sleep, Study and Relax"
+    document.getElementById("pause").classList.add("displayNone")
+    document.getElementById("play").classList.remove("displayNone")
 })
 
 btn4.addEventListener("click", () => {
     nameSong.innerHTML = "jazz/lofi hip hop radio🌱chill beats to relax/study to [LIVE 24/7]"
+    document.getElementById("pause").classList.add("displayNone")
+    document.getElementById("play").classList.remove("displayNone")
 })
 
