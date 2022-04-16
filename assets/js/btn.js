e4f2b994d801bbd6e4407e407f1676d3dceb59e0
ooAkira <lee.quockhanh1107@gmail.com>
2022-04-16 23:21:04 +0700

    fix bug background youtube and change music

--- assets/js/btn.js
+++ assets/js/btn.js
@@ -7,30 +7,34 @@ let bg = document.getElementById("background")
 let nameSong = document.getElementById("name-song");
 
 btn1.addEventListener("click", () => {
+    bg.src = "https://thumbs.gfycat.com/AcceptableGreedyDutchsmoushond-size_restricted.gif"
     nameSong.innerHTML = "lofi hip hop radio - beats to relax/study to"
     document.getElementById("pause").classList.add("displayNone")
     document.getElementById("play").classList.remove("displayNone")
-
+    youTubePlayerChangeVideoId()
 })
 
 btn2.addEventListener("click", () => {
+    bg.src = "https://i.gifer.com/AcU9.gif"
     nameSong.innerHTML = "coffee shop radio // 24/7 lofi hip-hop beats"
     document.getElementById("pause").classList.add("displayNone")
     document.getElementById("play").classList.remove("displayNone")
-    btn2.style.backgroundColor = 'red';
+    youTubePlayerChangeVideoId()
 })
 
 btn3.addEventListener("click", () => {
+    bg.src = "https://i.gifer.com/2swA.gif"
     nameSong.innerHTML = "Jazz Piano Music - Relaxing Slow Jazz 24/7 for Work, Sleep, Study and Relax"
     document.getElementById("pause").classList.add("displayNone")
     document.getElementById("play").classList.remove("displayNone")
-    btn3.style.backgroundColor = 'red';
+    youTubePlayerChangeVideoId()
 })
 
 btn4.addEventListener("click", () => {
+    bg.src = "https://i.gifer.com/ATEd.gif"
     nameSong.innerHTML = "jazz/lofi hip hop radio🌱chill beats to relax/study to [LIVE 24/7]"
     document.getElementById("pause").classList.add("displayNone")
     document.getElementById("play").classList.remove("displayNone")
-    btn4.style.backgroundColor = 'red';
+    youTubePlayerChangeVideoId()
 })
 
