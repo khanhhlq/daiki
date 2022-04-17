486d189eef5c9bf8c81f44fee33b8edf0000b74e
ooAkira <lee.quockhanh1107@gmail.com>
2022-04-17 09:00:38 +0700

    fix bug data

--- assets/js/btn.js
+++ assets/js/btn.js
@@ -1,21 +1,26 @@
-let backGround = {
-    name1: 'lofi hip hop radio - beats to relax/study to',
-    bg1: 'https://thumbs.gfycat.com/AcceptableGreedyDutchsmoushond-size_restricted.gif',
-    name2: 'coffee shop radio // 24/7 lofi hip-hop beats',
-    bg2: 'https://i.gifer.com/2swA.gif',
-    name3: 'Jazz Piano Music - Relaxing Slow Jazz 24/7 for Work, Sleep, Study and Relax',
-    bg3: 'https://thumbs.gfycat.com/AcceptableGreedyDutchsmoushond-size_restricted.gif',
-    name4: 'jazz/lofi hip hop radio🌱chill beats to relax/study to [LIVE 24/7]',
-    bg4: 'https://c.tenor.com/wch_imF_RLUAAAAC/lofi.gif'
-}
+let data = [
+    {
+        song1: '5qap5aO4i9A',
+        name1: 'lofi hip hop radio - beats to relax/study to',
+        bg1: 'https://thumbs.gfycat.com/AcceptableGreedyDutchsmoushond-size_restricted.gif',
+    },
+    {
+        song2: '-5KAN9_CzSA',
+        name2: 'coffee shop radio // 24/7 lofi hip-hop beats',
+        bg2: 'https://i.gifer.com/2swA.gif'
+    },
+    {
+        song3: 'DrmcAh2FRHQ',
 
-
-let idVideo = {
-    song1: '5qap5aO4i9A',
-    song2: '-5KAN9_CzSA',
-    song3: 'DrmcAh2FRHQ',
-    song4: 'kgx4WGK0oNU'
-}
+        name3: 'Jazz Piano Music - Relaxing Slow Jazz 24/7 for Work, Sleep, Study and Relax',
+        bg3: 'https://thumbs.gfycat.com/AcceptableGreedyDutchsmoushond-size_restricted.gif'
+    },
+    {
+        song4: 'kgx4WGK0oNU',
+        name4: 'jazz/lofi hip hop radio🌱chill beats to relax/study to [LIVE 24/7]',
+        bg4: 'https://c.tenor.com/wch_imF_RLUAAAAC/lofi.gif'
+    }
+]
 
 
 let btn1 = document.getElementById("btn-1")
@@ -24,42 +29,61 @@ let btn3 = document.getElementById("btn-3")
 let btn4 = document.getElementById("btn-4")
 let bg = document.getElementById("background")
 let ytb = document.getElementById('YouTube-video-id')
-
 let nameSong = document.getElementById("name-song");
 
 btn1.addEventListener("click", () => {
-    bg.src = backGround.bg1
-    ytb.value = idVideo.song1
-    nameSong.innerHTML = backGround.name1
+    bg.src = document.getElementById("preload1").src
+    setTimeout(() => {
+        bg.src = data[0].bg1
+    }, 500)
+    ytb.value = data[0].song1
+    nameSong.innerHTML = data[0].name1
     document.getElementById("pause").classList.add("displayNone")
     document.getElementById("play").classList.remove("displayNone")
     youTubePlayerChangeVideoId()
 })
 
 btn2.addEventListener("click", () => {
-    bg.src = backGround.bg2
-    ytb.value = idVideo.song2
-    nameSong.innerHTML = backGround.name2
+    bg.src = document.getElementById("preload2").src
+    setTimeout(() => {
+        bg.src = data[0].bg2
+    }, 500)
+    ytb.value = data[0].song2
+    nameSong.innerHTML = data[0].name2
     document.getElementById("pause").classList.add("displayNone")
     document.getElementById("play").classList.remove("displayNone")
     youTubePlayerChangeVideoId()
 })
 
 btn3.addEventListener("click", () => {
-    bg.src = backGround.bg3
-    ytb.value = idVideo.song3
-    nameSong.innerHTML = backGround.name3
+    bg.src = document.getElementById("preload1").src
+    setTimeout(() => {
+        bg.src = data[0].bg3
+    }, 500)
+    ytb.value = data[0].song3
+    nameSong.innerHTML = data[0].name3
     document.getElementById("pause").classList.add("displayNone")
     document.getElementById("play").classList.remove("displayNone")
     youTubePlayerChangeVideoId()
 })
 
 btn4.addEventListener("click", () => {
-    bg.src = backGround.bg4
-    ytb.value = idVideo.song4
-    nameSong.innerHTML = backGround.name4
+    bg.src = document.getElementById("preload2").src
+    setTimeout(() => {
+        bg.src = data[0].bg4
+    }, 500)
+    ytb.value = data[0].song4
+    nameSong.innerHTML = data[0].name4
     document.getElementById("pause").classList.add("displayNone")
     document.getElementById("play").classList.remove("displayNone")
     youTubePlayerChangeVideoId()
 })
+
+let previous = document.getElementById("previous")
+let forward = document.getElementById("forward")
+
+previous.addEventListener("click", () => {
+    idVideo--
+})
+// forward.addEventListener("click", forSong)
 
--- assets/js/changeId.js
+++
@@ -1,7 +0,0 @@
-import idVideo from "../data/idVideo"
-
-let previous = document.getElementById("previous")
-let forward = document.getElementById("forward")
-
-
-
