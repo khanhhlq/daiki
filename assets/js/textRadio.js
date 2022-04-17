9fd4c4c4512773d741efd2b85ba0a3b320671564
ooAkira <lee.quockhanh1107@gmail.com>
2022-04-17 09:56:38 +0700

    fix bug text raido 🚜

--- assets/js/textRadio.js
+++ assets/js/textRadio.js
@@ -3,15 +3,32 @@ let textData = [
     "💪 Hãy tiếp tục công việc đi nào!",
     "🔥 Không được trì hoãn",
     "🌊 Fighting",
+    "🔴 Đài phát thanh"
 ]
 
 let radio = document.getElementById("radio-text")
 
 let i = -5
 
+setTimeout(() => {
+    radio.innerHTML = textData[0]
+}, 7000)
 
+setTimeout(() => {
+    radio.innerHTML = textData[1]
+}, 20000)
 
+setTimeout(() => {
+    radio.innerHTML = textData[2]
+}, 30000)
 
+setTimeout(() => {
+    radio.innerHTML = textData[3]
+}, 40000)
+
+setTimeout(() => {
+    radio.innerHTML = textData[4]
+}, 100000)
 
 
 
