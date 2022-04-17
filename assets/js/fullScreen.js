744841a7659a8e66ce895be3f16eaaac561ec5f4
ooAkira <lee.quockhanh1107@gmail.com>
2022-04-17 09:48:20 +0700

    fix bug full screen

--- assets/js/fullScreen.js
+++ assets/js/fullScreen.js
@@ -3,10 +3,8 @@ let elem = document.getElementById("fullScreen");
 function openFullscreen() {
     var doc = window.document;
     var docEl = doc.documentElement;
-
     var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
     var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
-
     if (!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
         requestFullScreen.call(docEl);
     }
